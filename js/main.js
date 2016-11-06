11c2df10c150c5f5e6d77c5e107bdbc6f6f237ab
lauritha <lizamavergaralaura@gmail.com>
2016-11-06 18:27:52 -0300

    desarrollo de secciones

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
     $(".button-collapse").sideNav();
     $('.slider').slider({full_width: true});
-
-
-
-});
-
-$(window).scroll(function () {
-    // set distance user needs to scroll before we fadeIn navbar
-    if ($(this).scrollTop() > 600) {
-        $('nav').css("background-color","#494D4B");
-    } else {
-        $('nav').css("background-color","transparent");
-    }
 });
